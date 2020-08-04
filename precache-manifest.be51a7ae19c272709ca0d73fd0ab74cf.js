ab943164e627cde2b7e8486b46823a650c318887
abruckenstein <abruckenstein@tradepmr.com>
2020-08-03 20:58:33 -0400

    Updates

---
+++ precache-manifest.be51a7ae19c272709ca0d73fd0ab74cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70065dd4022717fd92305d2f436a0168",
+    "url": "/phase-tracker/index.html"
+  },
+  {
+    "revision": "b288b820b62afdd3ac3f",
+    "url": "/phase-tracker/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "96e6549de803e368b8c2",
+    "url": "/phase-tracker/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b288b820b62afdd3ac3f",
+    "url": "/phase-tracker/static/js/2.5ff902f7.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/phase-tracker/static/js/2.5ff902f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96e6549de803e368b8c2",
+    "url": "/phase-tracker/static/js/main.5bb3f025.chunk.js"
+  },
+  {
+    "revision": "a056f1aecc82b722d783",
+    "url": "/phase-tracker/static/js/runtime-main.25968644.js"
+  }
+]);
